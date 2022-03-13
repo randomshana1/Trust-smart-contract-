680c3d54d2d57f50884525d71562836913d95c37
shana <randomshana1@gmail.com>
2022-03-14 00:55:36 +0530

    added mapping and function

--- Trust.sol
+++ Trust.sol
@@ -1,14 +1,16 @@
 pragma solidity ^0.8.1;
 
 contract Trust {
-    mapping()
+    mapping(address => uint) public amounts;
+    mapping(address => uint) public maturities;
     address public kid;
     uint256 public maturity;
+    
 
-    constructor(address _kid, uint256 timeToMaturity) payable {
-        maturity = block.timestamp + timeToMaturity;
-        kid = _kid;
-    }
+    // constructor(address _kid, uint256 timeToMaturity) payable {
+    //     maturity = block.timestamp + timeToMaturity;
+    //     kid = _kid;
+    // }
 
     function withdraw() external {
         require(block.timestamp >= maturity, "too early");
