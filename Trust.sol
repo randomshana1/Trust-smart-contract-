2ed6e1e0f82449b096978848d198c215ce832b63
shana <randomshana1@gmail.com>
2022-03-14 01:12:01 +0530

    made some changes with mapping

--- Trust.sol
+++ Trust.sol
@@ -3,18 +3,27 @@ pragma solidity ^0.8.1;
 contract Trust {
     mapping(address => uint) public amounts;
     mapping(address => uint) public maturities;
-    address public kid;
-    uint256 public maturity;
-    
+    // address public kid;
+    // uint256 public maturity;
 
     // constructor(address _kid, uint256 timeToMaturity) payable {
     //     maturity = block.timestamp + timeToMaturity;
     //     kid = _kid;
     // }
 
-    function withdraw() external {
-        require(block.timestamp >= maturity, "too early");
-        require(msg.sender == kid, "only kid could withdraw");
-        payable(msg.sender).transfer(address(this).balance);
+    function addKid(address kid,uint timeToMaturity) external payable {
+        amounts[kid] =  msg.value;
+        maturities[kid] = block.timestamp + timeToMaturity;
+
+    } 
+
+    function withdraw(address kid) external {
+        require(maturities[kid] <= block.timestamp, "too early");
+        require(amounts[kid] > 0, "only kid could withdraw");
+
+        // require(block.timestamp >= maturity, "too early");
+        // require(msg.sender == kid, "only kid could withdraw");
+        // payable(msg.sender).transfer(address(this).balance);
+        payable(kid).transfer(amounts[kid]);
     }
 }
