9d410ecc004858cee516d190661ab73302ed8e78
shana <randomshana1@gmail.com>
2022-03-14 23:10:09 +0530

    added somehting

--- Trust.sol
+++ Trust.sol
@@ -1,8 +1,17 @@
 pragma solidity ^0.8.1;
 
+
 contract Trust {
+    struct kid {
+    uint amount;
+    uint maturity;
+    bool paid;
+    }
+    mapping (address => kid) public kids;
     mapping(address => uint) public amounts;
     mapping(address => uint) public maturities;
+    mapping (address=>bool) public paid;
+    address public admin;
     // address public kid;
     // uint256 public maturity;
 
@@ -10,8 +19,14 @@ contract Trust {
     //     maturity = block.timestamp + timeToMaturity;
     //     kid = _kid;
     // }
+    constructor() {
+        admin = msg.sender;
+
+    }
 
     function addKid(address kid,uint timeToMaturity) external payable {
+        require(msg.sender == admin, 'only admin');
+        require(amounts[msg.sender] == 0, "kid already exist");
         amounts[kid] =  msg.value;
         maturities[kid] = block.timestamp + timeToMaturity;
 
@@ -20,10 +35,11 @@ contract Trust {
     function withdraw(address kid) external {
         require(maturities[msg.sender] <= block.timestamp, "too early");
         require(amounts[msg.sender] > 0, "only kid could withdraw");
-
+        require(paid[msg.sender]== false, "paid already !");
+        paid[msg.sender] = true;
         // require(block.timestamp >= maturity, "too early");
         // require(msg.sender == kid, "only kid could withdraw");
         // payable(msg.sender).transfer(address(this).balance);
-        payable(kid).transfer(amounts[kid]);
+        payable(msg.sender).transfer(amounts[msg.sender]);
     }
 }
